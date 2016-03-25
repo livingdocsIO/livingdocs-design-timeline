d3dd3b568f6b9c8426108645c41b69a55fe778c9
Gabriel <gabriel.hase@gmail.com>
2016-03-25 16:11:09 +0100

    fix(cover): fix full-height in iframe

--- source/assets/scripts/full_height.js
+++ source/assets/scripts/full_height.js
@@ -1,17 +1,52 @@
 !function(){
-  var styleTag;
+  // hack to make sure Iframe Resizer is already loaded
+  setTimeout(function(){
 
-  var setStyleTag = function(height) {
-    styleTag = document.createElement('style')
-    styleTag.textContent = '.full-height {min-height: ' + height + 'px}'
-    styleTag.textContent += '.half-height {min-height: ' + height / 2 + 'px}'
-    document.head.appendChild(styleTag)
-  }
+    // definitions
+    var styleTag;
+    var setStyleTag = function(height) {
+      styleTag = document.createElement('style')
+      styleTag.textContent = '.full-height {min-height: ' + height + 'px}'
+      styleTag.textContent += '.half-height {min-height: ' + height / 2 + 'px}'
+      document.head.appendChild(styleTag)
+    }
+    var getViewportHeight = function(cb){
+      cb(null)
+    }
 
-  window.addEventListener('resize', function(){
-    styleTag.remove()
-    setStyleTag(window.innerHeight)
-  })
+    // in an Iframe with Iframe Resizer loaded
+    if(window.parentIFrame !== undefined) {
+      getViewportHeight = function(cb) {
+        window.parentIFrame.getPageInfo(function(data){
+          cb(data.clientHeight)
+        })
+      }
+    }
+    // in a general Iframe
+    else if(window.top != window.self) {
+      getViewportHeight = function(cb) {
+        cb(500)
+      }
+    }
+    // not in an Iframe
+    else {
+      getViewportHeight = function(cb) {
+        cb(window.innerHeight)
+      }
+    }
 
-  setStyleTag(window.innerHeight)
+    // initial setting of the height
+    getViewportHeight(function(viewportHeight){
+      setStyleTag(viewportHeight)
+    })
+
+    // setting on each resize event
+    window.addEventListener('resize', function(){
+      getViewportHeight(function(viewportHeight){
+        styleTag.remove()
+        setStyleTag(viewportHeight)
+      })
+    })
+
+  }, 10)
 }()
