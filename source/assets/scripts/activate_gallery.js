d8b8edf8b1eeeb62d6a5889a50eff8771dd47b7d
Gabriel <gabriel.hase@gmail.com>
2016-04-13 16:49:01 +0200

    fix(gallery): optimize loading for preview

--- source/assets/scripts/activate_gallery.js
+++ source/assets/scripts/activate_gallery.js
@@ -1,9 +1,22 @@
-if(window.liEditorLoaded) {
-  window.liEditorLoaded(function(liEditor){
-    if(liEditor.interactive) {
+var slider;
+var renderOrUpdateSlider = function(){
+  if(slider) {
+    slider.reloadSlider();
+  } else {
+    slider = $('.media-gallery-slides').slippry({auto: false, adaptiveHeight: false});
+  }
+}
+
+if(window.liEditor) {
+  window.liEditor.ready(function(editorInfo){
+    if(editorInfo.interactive) {
       // do nothing
     } else {
-      $('.media-gallery-slides').slippry({auto: false, adaptiveHeight: false});
+      renderOrUpdateSlider()
+      // listen to resize of preview pane
+      window.addEventListener('resize', function(){
+        renderOrUpdateSlider()
+      });
     }
   });
 } else {
